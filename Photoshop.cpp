1180d3728cf37bc1388e5260e37bba82afd004e7
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2022-04-22 21:52:47 +0200

    Add the main function name

--- Photoshop.cpp
+++ Photoshop.cpp
@@ -7,11 +7,130 @@
 
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cmath>
 #include "bmplib.cpp"
 
 using namespace std;
+unsigned char image[SIZE][SIZE];
+unsigned char image2[SIZE][SIZE];
+void loadImage ();
+void loadImage2 ();
+void saveImage ();
+void BlackAndWhiteImage ();
+void InvertImage () ;
+void Merge();
+void FlipImageHorizontal ();
+void FlipImageVertical ();
+void RotateImage () ;
+void Darken_Lighten ();
+void filterDetectEdges();
+void shrink ();
+void mirror ();
+void filterEnlargeImage ();
+void filterShuffleImage ();
+void blur () ;
+int getchoice();
+
+
 
 int main()
 {
-    
+    string choise ;
+    cout << "Ahlan ya user ya habibi \n";
+    loadImage ();
+    while (true)
+    {
+        cout << "Please select a filter to apply or 0 to exit: \n"
+                "  1-Black & White Filter \n"
+                "  2-Invert Filter \n"
+                "  3-Merge Filter \n"
+                "  4-Flip Image \n"
+                "  5-Rotate Image \n"
+                "  6-Darken and Lighten Image \n"
+                "  7-Detect Image Edges\n"
+                "  8-Enlarge image \n"
+                "  9-Shrink Image \n"
+                "  a-Mirror 1/2 Image \n"
+                "  b-Shuffle Image \n"
+                "  c-Blur Image \n"
+                "  s-Save the image to a file \n"
+                "  n-New Image \n"
+                "  0-Exit \n";
+        cin >> choise ;
+        if (choise == "1")
+        {
+            BlackAndWhiteImage ();
+        }
+        else if (choise == "2")
+        {
+            InvertImage ();
+        }
+        else if (choise == "3")
+        {
+            loadImage2 ();
+            Merge();
+        }
+        else if (choise == "4")
+        {
+            char enter ;
+            cout << "Flip (h)orizontally or (v)ertically ? \n";
+            cin >> enter ;
+            if (enter== 'h')
+                FlipImageHorizontal ();
+            if (enter = 'v')
+                FlipImageVertical ();
+        }
+        else if (choise == "5")
+        {
+            RotateImage () ;
+        }
+        else if (choise == "6")
+        {
+            Darken_Lighten ();
+        }
+        else if (choise == "7")
+        {
+            filterDetectEdges();
+        }
+        else if (choise == "8")
+        {
+            filterEnlargeImage ();
+        }
+        else if (choise=="9")
+        {
+            shrink();
+        }
+        else if (choise == "a")
+        {
+            mirror ();
+        }
+        else if (choise == "b")
+        {
+            filterShuffleImage ();
+        }
+        else if (choise == "c")
+        {
+            blur();
+        }
+        else if (choise == "s")
+        {
+            saveImage();
+        }
+        else if (choise == "n")
+        {
+            loadImage ();
+        }
+        else if (choise == "0")
+        {
+            break;
+        }
+        else
+        {
+            cout << "this input is invalid \n";
+        }
+    }
+    return 0;
 }
